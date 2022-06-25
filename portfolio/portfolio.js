01186bc1684192579e80d56e9f8a6f7136451f5a
WhizzyDoc <hassanridwan@gmail.com>
2022-06-25 08:46:53 +0100

    created a folder

---
+++ portfolio/portfolio.js
@@ -0,0 +1,30 @@
+function normal() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("profile").style.display = "none";
+}
+function home() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("profile").style.display = "none";
+}
+function work() {
+  document.getElementById("home").style.display = "none";
+    document.getElementById("work").style.display = "block";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("profile").style.display = "none";
+}
+function blog() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("blog").style.display = "block";
+    document.getElementById("profile").style.display = "none";
+}
+function profile() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("blog").style.display = "none";
+    document.getElementById("profile").style.display = "block";
+}
